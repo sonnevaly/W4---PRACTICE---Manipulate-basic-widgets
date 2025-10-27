90e893f4fbd512a7d4bf3920ef88b7de455cf9c6
sonnevaly <valygamer999@gmail.com>
2025-10-27 23:18:44 +0700

    add all exercise

---
+++ EX2/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Container(
+        padding: EdgeInsets.all(40),
+        margin: EdgeInsets.all(50),
+        decoration: BoxDecoration(color: Colors.blue[300],),
+        child: Container(
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(25), color: Colors.blue[600],),
+          child: Center(
+            child: Text('CADT STUDENTS', style: TextStyle(color: Colors.white)),
+          ),
+        ),
+      ),
+    ),
+  );
+}
