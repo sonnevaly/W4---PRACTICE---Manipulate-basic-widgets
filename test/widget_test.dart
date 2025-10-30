5a6b9f6ceb24add20f34207f88f93e3a4ccdf8d4
sonnevaly <valygamer999@gmail.com>
2025-10-30 09:31:30 +0700

    delete

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:myfirstapp/W4/EX1/main.dart';
+import 'package:myfirstapp/ex1.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
